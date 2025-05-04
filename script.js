b90105dd695392f7c5895697d5eef498362813c0
André <andremorais0000@gmail.com>
2025-05-04 19:12:31 -0300

    Rendering on screen values ​​added to form inputs

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const nameInput = document.getElementById("name");
 const notaInput = document.getElementById("nota");
+const divMensagem = document.querySelector("div");
 
 let listaNomeENota = [];
 
@@ -7,6 +8,16 @@ document.addEventListener("submit", (event) => {
     event.preventDefault()
     console.log("deu certo")
     listaNomeENota.push({name: nameInput.value, nota: notaInput.value},);
+    renderizarMensagem(listaNomeENota);
 })
 
-console.log(listaNomeENota)
+console.log(listaNomeENota);
+
+function renderizarMensagem(list) {
+    const codeMensagem = document.createElement("code");
+    console.log(listaNomeENota);
+    codeMensagem.innerHTML = JSON.stringify(list);
+    divMensagem.innerHTML = "";
+    divMensagem.appendChild(codeMensagem);
+}
+
